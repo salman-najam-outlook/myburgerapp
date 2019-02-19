b39ff6594961549bda789bbf8e6970211518ac72
Salman Najam <sa1man@msn.com>
2019-02-19 10:46:57 +0500

    AUX File name changed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/AuxTemp';
 import './OrderSummary.css';
 import Button from '../../UI/Button/Button';
 
